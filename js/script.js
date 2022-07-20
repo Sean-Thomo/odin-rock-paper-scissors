0548f12f7d456c510209265b9b8684ed31ca304c
siphesihle-thomo <seansthomo@gmail.com>
2022-07-20 17:39:08 +0200

    Add while loop to check if any player has 5 points

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function getComputerChoice () {
-const weapons = ['rock', 'paper', 'scissors'];
+  const weapons = ['rock', 'paper', 'scissors'];
   const number = Math.floor(Math.random() * weapons.length)
   const computerChoice = weapons[number]
   return computerChoice
@@ -9,6 +9,7 @@ function getPlayerChoice (){
   const buttons = document.querySelectorAll('.btn');
   let result = document.querySelector('.result');
 
+  // Call playRound() whenever a button is clicked
   buttons.forEach( btn => {
     btn.addEventListener('click', () => {
       let playerChoice = btn.id;
@@ -19,25 +20,28 @@ function getPlayerChoice (){
 }
 
 function playRound(playerChoice, computerChoice) {
-  // let computerScore = 0;
-  // let playerScore = 0;
+  let computerScore = 0;
+  let playerScore = 0;
 
-  switch (true) {
-    case playerChoice === computerChoice:
-      return 'Tie Game';
-    case playerChoice == 'rock' && computerChoice == 'paper':
-      return 'You lose! Paper beats Rock';
-    case playerChoice == 'paper' && computerChoice == 'scissors':
-      return 'You lose! Scissors beats Paper';
-    case playerChoice == 'scissors' && computerChoice == 'rock':
-      return 'You lose! Rock beats Scissors';
-    case computerChoice == 'scissors' && playerChoice == 'rock':
-      return 'You Win! Rock beats Scissors';
-    case computerChoice == 'rock' && playerChoice == 'paper':
-      return 'You Win! Paper beats Rock';
-    case computerChoice == 'paper' && playerChoice == 'scissors':
-      return 'You Win! Scissors beats Paper';
+  while (computerScore < 5 || playerChoice < 5){
+    switch (true) {
+      case playerChoice === computerChoice:
+        return 'Tie Game';
+      case playerChoice == 'rock' && computerChoice == 'paper':
+        return 'You lose! Paper beats Rock';
+      case playerChoice == 'paper' && computerChoice == 'scissors':
+        return 'You lose! Scissors beats Paper';
+      case playerChoice == 'scissors' && computerChoice == 'rock':
+        return 'You lose! Rock beats Scissors';
+      case computerChoice == 'scissors' && playerChoice == 'rock':
+        return 'You Win! Rock beats Scissors';
+      case computerChoice == 'rock' && playerChoice == 'paper':
+        return 'You Win! Paper beats Rock';
+      case computerChoice == 'paper' && playerChoice == 'scissors':
+        return 'You Win! Scissors beats Paper';
+    }
   }
+
 }
 
 function game() {
