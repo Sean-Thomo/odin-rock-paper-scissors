3f4fa8fd76d185dee9154ffd00036bbb05963228
siphesihle-thomo <seansthomo@gmail.com>
2022-07-20 16:35:13 +0200

    Add eventListener for btn selection

--- js/script.js
+++ js/script.js
@@ -6,7 +6,12 @@ const weapons = ['rock', 'paper', 'scissors'];
 }
   
 function getPlayerChoice (){
-  const playerChoice = prompt("Choose Your Weapon: Rock, Paper or Scissors?", "Rock").toLowerCase()
+  // const playerChoice = prompt("Choose Your Weapon: Rock, Paper or Scissors?", "Rock").toLowerCase()
+  const buttons = document.querySelectorAll('.btn');
+  buttons.forEach( btn => {
+    let playerChoice = btn.id
+    btn.addEventListener('click', playRound())
+  })
   return playerChoice
 }
 
@@ -17,25 +22,18 @@ function playRound() {
   switch (true) {
     case playerChoice === computerChoice:
       return 'Tie Game';
-      break;
     case playerChoice == 'rock' && computerChoice == 'paper':
       return 'You lose! Paper beats Rock';
-      break;
     case playerChoice == 'paper' && computerChoice == 'scissors':
       return 'You lose! Scissors beats Paper';
-      break;
     case playerChoice == 'scissors' && computerChoice == 'rock':
       return 'You lose! Rock beats Scissors';
-      break;
     case computerChoice == 'scissors' && playerChoice == 'rock':
       return 'You Win! Rock beats Scissors';
-      break;
     case computerChoice == 'rock' && playerChoice == 'paper':
       return 'You Win! Paper beats Rock';
-      break;
     case computerChoice == 'paper' && playerChoice == 'scissors':
       return 'You Win! Scissors beats Paper';
-      break;
   }
 }
 
@@ -46,13 +44,3 @@ function game() {
 }
 
 // game()
-const log = () => {
-  const buttons = document.querySelectorAll('.btn');
-  buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-      alert(btn.id);
-    })
-  })
-}
-
-log()
