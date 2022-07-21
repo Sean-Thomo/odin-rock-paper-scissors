07bcbd35aba4c220d7ee6bc4ec1e9e7a6e2a1ab9
siphesihle-thomo <seansthomo@gmail.com>
2022-07-21 18:29:08 +0200

    Add pickWinner and reset game function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice () {
   const weapons = ['rock', 'paper', 'scissors'];
   const number = Math.floor(Math.random() * weapons.length)
@@ -13,47 +16,75 @@ function getPlayerChoice (){
   buttons.forEach( btn => {
     btn.addEventListener('click', () => {
       let playerChoice = btn.id;
-      let computerChoice = getComputerChoice()
-      result.innerHTML = playRound(playerChoice, computerChoice)
+      let computerChoice = getComputerChoice();
+      result.innerHTML = playRound(playerChoice, computerChoice);
+      
+      if (playerScore === 5 || computerScore ===5){
+        pickWinner();
+        resetGame();
+      }
     })
   })
+
 }
 
 function playRound(playerChoice, computerChoice) {
-  let computerScore = 0;
-  let playerScore = 0;
-
   while (computerScore < 5 || playerScore < 5){
     switch (true) {
       case playerChoice === computerChoice:
         return 'Tie Game';
       case playerChoice == 'rock' && computerChoice == 'paper':
-        computerScore ++;
+        computerScore += 1;
+        displayComputerScore()
         return 'You lose! Paper beats Rock';
       case playerChoice == 'paper' && computerChoice == 'scissors':
-        computerScore ++;
+        computerScore += 1;
+        displayComputerScore()
         return 'You lose! Scissors beats Paper';
       case playerChoice == 'scissors' && computerChoice == 'rock':
-        computerScore ++;
+        computerScore += 1;
+        displayComputerScore()
         return 'You lose! Rock beats Scissors';
       case computerChoice == 'scissors' && playerChoice == 'rock':
-        playerScore ++;
+        playerScore += 1;
+        displayPlayerScore()
         return 'You Win! Rock beats Scissors';
       case computerChoice == 'rock' && playerChoice == 'paper':
-        playerScore ++;
+        playerScore += 1;
+        displayPlayerScore()
         return 'You Win! Paper beats Rock';
       case computerChoice == 'paper' && playerChoice == 'scissors':
-        playerScore ++;
+        playerScore += 1;
+        displayPlayerScore()
         return 'You Win! Scissors beats Paper';
     }
   }
 
+}
+
+function pickWinner(){
   if (playerScore > computerScore) {
     return 'We WON! We\'ll witness a few more trips around the sun'
   } else {
     return 'We LOST! No more listening to Kendrick and playing video games'
   }
+}
 
+function resetGame() {
+  playerScore= 0;
+  computerScore =0;
+  displayComputerScore()
+  displayPlayerScore()
+}
+
+function displayPlayerScore(){
+  let playerDisplay = document.querySelector('#playerScore');
+  playerDisplay.textContent = playerScore;
+}
+
+function displayComputerScore(){
+  let computerDisplay = document.querySelector('#computerScore');
+  computerDisplay.textContent = computerScore;
 }
 
 function game() {
